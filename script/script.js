e592126460a5dbbd1ecd6463a1e8d7febc858f25
unknown <naimurrahman8392@gmail.com>
2024-02-19 22:23:52 +0600

    final commit

--- script/script.js
+++ script/script.js
@@ -1,49 +1,142 @@
-const buttn = document.getElementsByClassName('button')
-for( const btn of buttn){
 
-    let count = 0;
-    let tk = 0;
-    let available = 32 ;
-    let buttonsClicked = {};
-    
-    const buttons = document.getElementsByClassName('button');
-    for (const btn of buttons) {
-        btn.addEventListener('click', function() {
-            // Check if button has already been clicked and count is less than 4
-            if (!buttonsClicked[btn.innerText] && count < 4) {
-                count = count + 1;
-                tk = count * 550;
-                available = available - 1;
-                document.getElementById('cnt').innerText = count;
-                document.getElementById('tk').innerText = tk;
-                document.getElementById('grand-total').innerText = tk ; 
-                document.getElementById('available').innerText = available;
-                btn.style.backgroundColor = "green";
+document.getElementById('first-click').addEventListener('click', function () {
+    var element = document.getElementById('second-click');
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
 
-                buttonsClicked[btn.innerText] = true; // Mark button as clicked
-            }
-        });
+
+
+ document.getElementById("A1").addEventListener("click", function(event){
+     console.log("click");
+    console.log(event)
+});
+
+document.addEventListener('click', handleClickEvent)
+
+ function handleClickEvent(event) {
+    console.log(event)
+    console.log(event.target.id)
+    const id1 = event.target.id
+   colorEnable(id1)
+
+document.addEventListener('click', handleClickEvent)
+
+function handleClickEvent(event) {
+    const selectedIds = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'E4', 'F1', 'F2', 'F3', 'F4', 'G1', 'G2', 'G3', 'G4', 'H1', 'H2', 'H3', 'H4',];
+
+    if (selectedIds.includes(event.target.id)) {
+        const id = event.target.id;
+        colorEnable(id);
     }
 }
 
 
+let count = 0
+let seatCount = 40
+let totalPrice = 0
+let grandTotal = 0
+function colorEnable(m) {
+    const phoneNumberInput = document.getElementById('phone-number');
 
+    count += 1
+    if (count <= 4) {
+        setBackgroundColorById(m)
+        disabledElementById(m)
 
- document.getElementById('coupon').addEventListener('keyup', function(event){
-  const text = event.target.value
-    const dlt = document.getElementById('btn-apply')
-   if(text == '15% OFF'){
-       dlt.removeAttribute('disabled')
-       const offTk = tk * 0.8
+       
+        seatCount = seatCount - 1;
+        seatCount1('seat-count')
+
         
-   }
-    else{
-        dlt.setAttribute('disabled', true)
-    }
-})
+        ticketName1("price-div", m)
 
-document.getElementById('btn-apply').addEventListener('click', function(){
-   const grandTotal = document.getElementById('grand-total')
-    
-     secret.style.display = 'none' 
-    })
+     
+        totalPrice += 550
+        totalPrice1('total-price')
+
+        if (count < 4) {
+            grandTotal += 550
+            totalPrice1('grand-total')
+
+        }
+        else {
+          
+
+            grandTotal1('grand-total')
+
+
+
+        }
+
+
+        phoneNumberInput.addEventListener('input', function() {
+            if (count > 0 && containsNumbers(this.value)) {
+                const btn = document.getElementById('next-button');
+                btn.disabled = false;
+            }
+        });
+
+    }
+    else {
+        disabledElementById(m)
+
+    }
+
+
+}
+
+
+
+function containsNumbers(input) {
+    const digitRegex = /\d/;
+    return digitRegex.test(input);
+}
+
+function ticketName1(n, m) {
+    const divSection = document.createElement("div");
+    divSection.innerHTML =
+        <p> ${m}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Economy &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 550</p>;
+
+
+    document.getElementById(n).appendChild(divSection);
+}
+
+function seatCount1(m) {
+    const id = document.getElementById(m)
+    id.innerText = seatCount
+}
+function totalPrice1(m) {
+    const id = document.getElementById(m)
+    id.innerText = totalPrice
+}
+function grandTotal1(m) {
+    grandTotal = grandTotal + 550
+    console.log(grandTotal)
+    let discount = grandTotal * .15
+    console.log(discount)
+    grandTotal = grandTotal - discount
+    const id = document.getElementById(m)
+    id.innerText = grandTotal
+}
+
+function disabledElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.disabled = true
+
+}
+
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-green-400');
+}
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-green-400');
+}
